b2e051d01de0dabbf341f034113f2bc25cd7509d
wla588 <12301017@bjtu.edu.cn>
2015-06-05 22:59:27 +0800

    支付发布第一个版本
    支付第一个版本

---
+++ Project/王里奥/开发库/支付/支付公共类/I-MyStrutsandHibernate-5.1/src/pojo/Receipt.java
@@ -0,0 +1,42 @@
+package pojo;
+
+
+public class Receipt {
+	
+	private int id;
+	private long date;
+	private int state;
+	private float cost;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+	public long getDate() {
+		return date;
+	}
+	public void setDate(long date) {
+		this.date = date;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	
+	
+	
+
+}
